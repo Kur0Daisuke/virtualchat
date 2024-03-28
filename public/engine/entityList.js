3abb123d1a049f8ec673b76c78b162de59b37e18
SakutaTHZ <thzarts@gmail.com>
2024-03-28 18:24:15 +0630

    major changes, scrapping the old code

--- public/engine/entityList.js
+++
@@ -1,9 +0,0 @@
-class EntityList{
-    static #Entities = null;
-
-    static set SetEntities(data) {
-        EntityList.#Entities = Object.keys(data).map((key) => data[key]);
-    }
-
-    static get Entites() { return EntityList.#Entities }
-}
